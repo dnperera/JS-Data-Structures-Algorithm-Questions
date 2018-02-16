527599620cd41f4a3349537ff238e1d112910ce0
Dayan Perera <dayannp@gmail.com>
2018-02-15 21:58:46 -0800

    Reverse given string with HOF

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,11 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// function reverse(str) {
+// 	return str.split('')
+// 	.reverse()
+// 	.join('');
+// }
+//using HOF 
 function reverse(str) {
 	return str.split('')
-	.reverse()
-	.join('');
+	.reduce((reversedStr ,str) =>str.concat(reversedStr),'');
 }
+//ES6 loop solution
+// function reverse(str) {
+// 	var reversedStr ="";
+// 	for( let character of str){
+// 		reversedStr = character + reversedStr;
+// 	}
+// 	return reversedStr;
+// }
 
+
+// Solution with traditional for loop
+
+// function reverse(str) {
+// 	var reversedStr ="";
+// 	for( let i=0; str.length > i; i++) {
+// 		reversedStr = str[i] + reversedStr;
+// 	}
+// 	return reversedStr;
+// }
 console.log(reverse('apple'));
 module.exports = reverse;
