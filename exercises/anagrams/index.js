5b81fbd83f7912452caf8fc4c809f4b4d4cea984
Dayan Perera <dayannp@gmail.com>
2018-03-08 19:32:31 -0800

    Check provide strings are anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,21 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+	//Create letters only array for both strings using regular expressions
+	var letters = /[A-Z0-9]/gi; // Reg Expression
+	var stringAArray = stringA.match(letters);
+	var stringBArray = stringB.match(letters);
+	//Sort both array alphabetically
+	stringAArray.sort(function(a, b) {
+		return a > b;
+	});
+	stringBArray.sort(function(a, b) {
+		return a > b;
+	});
 
+	//now join both array back to the string. check both equal
+	return stringAArray.join('').toLowerCase() === stringBArray.join('').toLowerCase();
+}
+console.log(anagrams('Hi there', 'Bye there'));
 module.exports = anagrams;
