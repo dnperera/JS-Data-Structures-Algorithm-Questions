f2a5c177624b6211f508a21f6d2f34b9c3000b65
Dayan Perera <dayannp@gmail.com>
2018-04-25 18:51:57 -0700

    add contains method to stack

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -11,21 +11,67 @@
 //   s.pop(); // returns 1
 
 class Stack {
-	constructor() {
-		this.data = [];
+	constructor(capacity) {
+		this.capacity = capacity || Infinity;
+		this.data = {};
+		this.count = 0;
 	}
 
 	push(record) {
-		this.data.push(record);
+		if (this.count < this.capacity) {
+			this.data[this.count++] = record;
+			return this.count;
+		}
+		return 'Max capacity already reached. Remove element before adding new element';
 	}
 
 	pop() {
-		return this.data.pop();
+		if (this.count > 0) {
+			let temp = this.data[--this.count];
+			delete this.data[this.count];
+			return temp;
+		}
+		return undefined;
 	}
 
 	peek() {
-		return this.data[this.data.length - 1];
+		return this.data[this.count - 1];
+	}
+
+	contains(record) {
+		const holdingStack = new Stack();
+		let temp = '';
+		let isRecordFound = false;
+
+		//pop each value of the current stack and push to holding stack untill given value find
+		while (this.peek()) {
+			temp = this.pop();
+			holdingStack.push(temp);
+			if (temp === record) {
+				isRecordFound = true;
+				break;
+			}
+		}
+
+		//push values back to the original stack
+		while (holdingStack.peek()) {
+			this.push(holdingStack.pop());
+		}
+
+		return isRecordFound;
+	}
+
+	count() {
+		return this.count;
 	}
 }
 
+// const s = new Stack(5);
+// s.push('Dayan');
+// s.push('Hasini');
+// s.push('Denith');
+// s.push('Dasith');
+
+// console.log(s.contains('Denith'));
+// console.log(s);
 module.exports = Stack;
